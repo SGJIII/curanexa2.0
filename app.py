4db791fcfacdaff0eb719f0978a40be2aa0fe254
Sam Johnson <samueljoh@gmail.com>
2023-10-17 01:28:11 -0400

    added heroku and requirements and Procfile

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template, redirect, url_for
+import torchxrayvision as xrv
+import os
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-@app.route('/')
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Hello, World!"
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(filepath)
+            # Process the image with torchxrayvision here
+            # For now, we'll just redirect to the home page
+            return redirect(url_for('index'))
+    return render_template('upload.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
